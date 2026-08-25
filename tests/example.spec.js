720c16ba8ec295ad6b31f1471e8ec720f9840a9d
Penya123 <jorgesagt5@gmail.com>
2026-08-25 00:57:11 -0700

    Se terminó el desarrollo del test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,11 @@
 const { test, expect } = require("@playwright/test");
-const { count } = require("node:console");
-
-
 
 test('Test Kanban Web App', async ({ page }) => {
 
     // Ir a la página
     await page.goto("https://kanban-566d8.firebaseapp.com/")
 
-    const createTask = async () => { // Crear tasks
+    const createTask = async () => { // Crea una task
         await page.locator("button.hidden").click();
 
         await page.locator("#Title").fill("Ramdom task");
@@ -21,25 +18,84 @@ test('Test Kanban Web App', async ({ page }) => {
 
         await page.locator("button[type=submit]").click();
     }
-    console.log(await page.locator("section").nth(1).locator("article").count())
+
+    const checkSubtask = async (task) => { // completa una subtask
+        await secondColumn.nth(task).click();
+
+        const subtasks = page.locator("div label span");
+        for (let j = 0; j < await subtasks.count(); j++) {
+
+            const tachado = await subtasks.nth(j).evaluate(
+                el => getComputedStyle(el).textDecorationLine.includes("line-through")
+            );
+
+            if (!tachado) {
+                await subtasks.nth(j).click(); // lo marca como completado
+                await expect(subtasks.nth(j)).toHaveCSS("text-decoration-line", "line-through");
+                break;
+            }
+        }
+
+        // Abrir selector de columnas y elegir la primera
+        await page.locator("div.text-sm").nth(2).click();
+        await page.locator("div div.hidden.absolute.rounded div").nth(0).click();
+
+        await page.mouse.click(10, 10); // Salirse de la página de edición de la task
+
+        // Conseguir las subtast desde el texto
+        const [complitedSubtasks, totalTasks] = (await page.locator("section").first().locator("article").last().textContent()).match(/\d+/g).map(Number);
+
+        return complitedSubtasks;
+    }
+
+    const secondColumn = page.locator("section").nth(1).locator("article")
+    const secondColumnTasks = await secondColumn.count();
+    console.log("Tasks in the second column:", secondColumnTasks);
 
     // Si no hay tasks en la secunda comlumna, crear una
-    if (await page.locator("section").nth(1).locator("article").count() === 0) {
-        createTask(); // Si no hay articulos se llama a la función para crear uno
+    if (await secondColumnTasks === 0) {
+        await createTask(); // Si no hay task se llama a la función para crear uno
     }
+
 
     // Iterar en las tasks de la segunda columna para encontrar una que no tenga sus subtasks completadas
-    let i = 0;
-    const secondColumnTasks = await page.locator("section").nth(1).locator("article").count();
+    let complited = 0, total = 0, i = 0;
+    let taskName = ""
     for (i; i < secondColumnTasks; i++) {
-        const subtasks = await page.locator("section").nth(1).locator("article").nth(i).textContent();
+        const subtasks = await secondColumn.nth(i).textContent();
 
-        const [completadas, total] = subtasks.match(/\d+/g).map(Number);
+        // Conseguir las subtast desde el texto
+        [complited, total] = subtasks.match(/\d+/g).map(Number);
 
-        if (completadas === total) {
-
+        if (complited !== total) {
+            taskName = (await secondColumn.nth(i).locator("h3").textContent()).trim();
+            break;
         }
     }
-    await page.pause()
+
+    // await page.pause();
+
+    let finalSubtasks = 0
+
+    if (i >= secondColumnTasks) { // Si todas las tasks están completadas, crea una nueva
+        await createTask();
+        taskName = "Ramdom task";
+        finalSubtasks = await checkSubtask(i++);
+    } else {
+        finalSubtasks = await checkSubtask(i);
+    }
+
+    console.log("the original subtasks were: ", complited);
+    console.log("the complited subtasks were: ", finalSubtasks);
+    expect(++complited === finalSubtasks).toBeTruthy();
+
+    const firstColumn = page.locator("section").first();
+
+    // Verificar que la carta con ese título ahora vive dentro de la primera columna
+    await expect(
+        firstColumn.locator("article", { hasText: taskName })
+    ).toBeVisible();
+
+    // await page.pause();
 })
 
